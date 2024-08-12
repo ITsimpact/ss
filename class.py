9c017f3b90e5a863997d99a1a90fb6c405fd970b
Aziz <sobirjonov0909@gmail.com>
2024-08-12 21:06:19 +0500

    ss

---
+++ class.py
@@ -0,0 +1,34 @@
+#1
+# a = int(input('vedite cislo: '))
+# if a % 2 == 0:
+#     print('cislo cetnoe')
+# else:
+#     print('cislo necetnoe')
+#2
+# A = []
+# for _ in range(5):
+#     a = int(input('Введите целое число...'))
+#     A.append(int(a))
+# m_min = A[0]
+# for i in A:
+#     if m_min > i:
+#         m_min = i
+# print(f'Минимальное: {m_min}' )
+#3
+# a = str(input())
+# b = a[::-1]
+# if a == b:
+#   print("yes")
+# else:
+#   print("no")
+#4
+# a = int(input('Введите количество чисел в последовательности: '))
+# numbers = []
+# for i in range(0, a):
+#     input_value = int(input(f'Введите число #{i}: '))
+#     numbers.append(input_value)
+# sum = 0
+# for i in numbers:
+#     sum += i
+# print('Сумма всех чисел последовательности:', sum)
+#5
